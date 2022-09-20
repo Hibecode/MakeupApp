7545d185c5e5a5f91470887d60b5cdc3b137f561
Hibecode <ibkokunoye@gmail.com>
2022-09-20 19:26:55 +0100

    Add makeup product layout

--- app/src/main/java/com/example/makeupapp/adapters/ParentBrandAdapter.kt
+++ app/src/main/java/com/example/makeupapp/adapters/ParentBrandAdapter.kt
@@ -1,3 +1,4 @@
+/*
 package com.example.exhibitapp.adapters
 
 import android.view.LayoutInflater
@@ -7,18 +8,18 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.exhibitapp.R
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.parent_recyclerview.view.*
-import com.example.exhibitapp.models.ExhibitItem
+import com.example.makeupapp.R
+import com.example.makeupapp.databinding.ProductRecyclerviewBinding
+import com.example.makeupapp.databinding.ProductRecyclerviewItemBinding
 
 
 // Parent Adapter for the vertical scroll list of images
-class ParentBrandAdapter: RecyclerView.Adapter<ParentBrandAdapter.TitleViewHolder>() {
+class ParentBrandAdapter: RecyclerView.Adapter<ParentBrandAdapter.ParentBrandAdapter>() {
 
-    inner class TitleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ParentBrandAdapter(val binding: ProductRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private val differCallback = object: DiffUtil.ItemCallback<ExhibitItem>() {
+    */
+/*private val differCallback = object: DiffUtil.ItemCallback<ExhibitItem>() {
         override fun areItemsTheSame(oldItem: ExhibitItem, newItem: ExhibitItem): Boolean {
             return oldItem.title == newItem.title
         }
@@ -29,25 +30,24 @@ class ParentBrandAdapter: RecyclerView.Adapter<ParentBrandAdapter.TitleViewHolde
 
     }
 
-    val differ = AsyncListDiffer(this, differCallback)
+    val differ = AsyncListDiffer(this, differCallback)*//*
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TitleViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.parent_recyclerview, parent, false)
 
-        return TitleViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParentBrandAdapter {
+        return ParentBrandAdapter(ProductRecyclerviewBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false))
     }
 
-    override fun getItemCount() = differ.currentList.size
+    override fun getItemCount() = //differ.currentList.size
 
-    override fun onBindViewHolder(holder: TitleViewHolder, position: Int) {
-        val exhibitItem = differ.currentList[position]
-        val childAdapter = ChildProductAdapter(exhibitItem.images as ArrayList<String>)
-        holder.itemView.apply {
-            tvTitle.text = exhibitItem.title
-            rvChild.layoutManager = LinearLayoutManager(holder.itemView.context, LinearLayoutManager.HORIZONTAL, false)
-            rvChild.adapter = childAdapter
+    override fun onBindViewHolder(holder: ParentBrandAdapter, position: Int) {
+        //val brandItem = differ.currentList[position]
+        //val childAdapter = ChildProductAdapter(exhibitItem.images as ArrayList<String>)
+        holder.binding.apply {
+            //tvTitle.text = exhibitItem.title
+            rvChild.layoutManager = LinearLayoutManager(holder.binding.root.context, LinearLayoutManager.HORIZONTAL, false)
+            //rvChild.adapter = childAdapter
         }
     }
 
-}
+}*/
