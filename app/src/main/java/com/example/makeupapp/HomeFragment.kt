01a065e8fefcd08f1400366fe1c6aaa7284328e1
Hibecode <ibkokunoye@gmail.com>
2022-09-20 13:16:50 +0100

    Add ViewBinding to home fragment

--- app/src/main/java/com/example/makeupapp/HomeFragment.kt
+++ app/src/main/java/com/example/makeupapp/HomeFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.makeupapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater,container, false)
+        val view = binding.root
+        return view
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
